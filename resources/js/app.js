29178af625233a0745ac29e87848f298d4083837
Muhammad Irwan Andriawan <irwan@andriawan.com>
2021-08-23 00:42:46 +0700

    page(landing): add simple ui and chunk file upload client side logic
    - styling with progress bar (powered by bootstrap framework)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,53 @@
 require('./bootstrap');
+
+let errorMessage = $("#error-message");
+let successMessage = $("#success-message");
+let progressBarUpload = $("#progress-bar-container");
+
+$("#file-upload").change(function (event) {
+    errorMessage.addClass("d-none");
+    if (event.currentTarget.files[0].name){
+        $("#label-file-name").text(event.currentTarget.files[0].name);
+    }else{
+        $("#label-file-name").text("");
+    }
+})
+
+$("#button-upload").click(function () {
+    let files = $("#file-upload").get(0).files[0]
+    let chunkSize = 100000;
+
+    errorMessage.addClass("d-none");
+    successMessage.addClass("d-none");
+    progressBarUpload.addClass("d-none");
+
+
+    if(files){
+        let loaded = 0;
+        let reader = new FileReader();
+        let blob = files.slice(loaded, chunkSize);
+        reader.readAsArrayBuffer(blob);
+        reader.onload = function(e) {
+            loaded += chunkSize;
+            let valueProgress = Math.round((loaded/files.size) * 100);
+            if(valueProgress > 100) valueProgress = 100;
+            progressBarUpload
+                .removeClass("d-none")
+                .find(".progress-bar").css("width",  `${valueProgress}%`)
+                .text(`${valueProgress}%`)
+            if (loaded < files.size) {
+                blob = files.slice(loaded, loaded + chunkSize);
+                setTimeout(function () {
+                    reader.readAsArrayBuffer(blob);
+                }, 400)
+            }else{
+                successMessage.removeClass("d-none").text("Upload File Success!");
+                progressBarUpload.addClass("d-none");
+            }
+        }
+
+    }else{
+        errorMessage.removeClass("d-none").text("Please select a mp4 file");
+    }
+    console.log(files);
+})
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -9,7 +9,29 @@
     <link rel="stylesheet" href={{ asset('css/app.css') }}>
 </head>
 <body>
-    <p>Test</p>
+    <div class="container mt-4">
+        <div class="jumbotron bg-secondary text-white">
+            <h1 class="display-4">AndLaravel Video Streaming</h1>
+            <p class="lead">Hello, this is my experimental Apps in learning building Video Streaming Simple Apps</p>
+            <hr class="my-4">
+            <p>This is very-very experimental. Just use for fun only!</p>
+            <div id="error-message" class="d-none alert alert-danger" role="alert">
+            </div><div id="success-message" class="d-none alert alert-success" role="alert">
+            </div>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <button id="button-upload" class="btn btn-primary" type="button">Upload</button>
+                </div>
+                <div class="custom-file">
+                    <input id="file-upload" accept="video/mp4" type="file" class="custom-file-input" id="inputGroupFile01">
+                    <label id="label-file-name" class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                </div>
+            </div>
+            <div id="progress-bar-container" class="progress d-none">
+                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0%</div>
+            </div>
+        </div>
+    </div>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
