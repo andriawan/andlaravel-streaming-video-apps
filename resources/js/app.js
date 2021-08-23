fc9103f13203785edd83146cd82e84419e6e5eab
Muhammad Irwan Andriawan <irwan@andriawan.com>
2021-08-23 22:12:21 +0700

    add(improvement): better logic upload chunk handler

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,51 +3,90 @@ require('./bootstrap');
 let errorMessage = $("#error-message");
 let successMessage = $("#success-message");
 let progressBarUpload = $("#progress-bar-container");
+let fileUploadElement = $("#file-upload");
+let buttonUploadElement = $("#button-upload");
+let labelFileName = $("#label-file-name");
 
-$("#file-upload").change(function (event) {
+fileUploadElement.change(function (event) {
     errorMessage.addClass("d-none");
-    if (event.currentTarget.files[0].name){
-        $("#label-file-name").text(event.currentTarget.files[0].name);
-    }else{
-        $("#label-file-name").text("");
+    if (event.currentTarget.files[0].name) {
+        labelFileName.text(event.currentTarget.files[0].name);
+    } else {
+        labelFileName.text("");
     }
 })
 
-$("#button-upload").click(function () {
-    let files = $("#file-upload").get(0).files[0]
-    let chunkSize = 100000;
-
-    errorMessage.addClass("d-none");
-    successMessage.addClass("d-none");
-    progressBarUpload.addClass("d-none");
-
-
-    if(files){
-        let loaded = 0;
-        let reader = new FileReader();
-        let blob = files.slice(loaded, chunkSize);
-        reader.readAsArrayBuffer(blob);
-        reader.onload = function(e) {
-            loaded += chunkSize;
-            let valueProgress = Math.round((loaded/files.size) * 100);
-            if(valueProgress > 100) valueProgress = 100;
-            progressBarUpload
-                .removeClass("d-none")
-                .find(".progress-bar").css("width",  `${valueProgress}%`)
-                .text(`${valueProgress}%`)
-            if (loaded < files.size) {
-                blob = files.slice(loaded, loaded + chunkSize);
-                setTimeout(function () {
-                    reader.readAsArrayBuffer(blob);
-                }, 400)
-            }else{
-                successMessage.removeClass("d-none").text("Upload File Success!");
-                progressBarUpload.addClass("d-none");
+function sendChunkFile(data) {
+    let form = new FormData()
+    form.append("video", new File([data.reader.result], "uploaded_video"))
+    $.ajax({
+        type: "POST",
+        data: form,
+        contentType: false,
+        processData: false,
+        url: "/api/video",
+        success: function () {
+            if (data.loaded < data.files.size) {
+                data.loaded += data.chunkSize;
+                console.log("dt", data);
+                let valueProgress = Math.round((data.loaded / data.files.size) * 100);
+                if (valueProgress > 100) valueProgress = 100;
+                data.progressBarUpload
+                    .removeClass("d-none")
+                    .find(".progress-bar").css("width", `${valueProgress}%`)
+                    .text(`${valueProgress}%`)
+                data.blob = data.files.slice(data.loaded, data.loaded + data.chunkSize);
+                data.reader.readAsArrayBuffer(data.blob);
+            } else {
+                data.successMessage.removeClass("d-none").text("Upload File Success!");
+                data.progressBarUpload.addClass("d-none");
+                data.buttonUploadElement.prop("disabled", false);
+                data.fileUploadElement.prop("disabled", false).val('');
+                data.labelFileName.text("");
             }
         }
+    })
 
-    }else{
-        errorMessage.removeClass("d-none").text("Please select a mp4 file");
+
+}
+
+buttonUploadElement.click(function () {
+
+    let data = {
+        loaded: 0,
+        reader: new FileReader(),
+        chunkSize: 1048576,
+        errorMessage,
+        successMessage,
+        progressBarUpload,
+        fileUploadElement,
+        buttonUploadElement,
+        labelFileName
     }
-    console.log(files);
+
+    data.buttonUploadElement.prop("disabled", true);
+    data.fileUploadElement.prop("disabled", true);
+    data.errorMessage.addClass("d-none");
+    data.successMessage.addClass("d-none");
+    data.progressBarUpload.addClass("d-none");
+
+    data.files = data.fileUploadElement.get(0).files[0];
+
+
+    if (data.files) {
+        data.progressBarUpload
+            .removeClass("d-none")
+            .find(".progress-bar").css("width", "100%")
+            .text("Uploading...");
+        data.blob = data.files.slice(data.loaded, data.chunkSize);
+        data.reader.readAsArrayBuffer(data.blob);
+        data.reader.onload = function (e) {
+            sendChunkFile(data)
+        }
+
+    } else {
+        data.errorMessage.removeClass("d-none").text("Please select a mp4 file");
+        data.buttonUploadElement.prop("disabled", false);
+        data.fileUploadElement.prop("disabled", false)
+    }
 })
